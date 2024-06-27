158e68699d00617ffbbc5ed9304070327707d00f
Katrine Alice Glasscock <katrine.glasscock@manchester.ac.uk>
2024-06-27 11:48:01 +0100

    fixed typo for commandline argument for toggling cosmic variance

--- run_sampler.sh
+++ run_sampler.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 50
-#SBATCH --job-name diff_sampler
-#SBATCH --array=1#-40 #Run 40 copies of the code = 4000 samples
+#SBATCH --job-name standard_a0
+#SBATCH --array=1 #-40 #Run 40 copies of the code = 4000 samples
 #SBATCH --partition cosma8-serial
 #SBATCH --account dp270
 #SBATCH --output /cosma8/data/dp270/dc-glas1/slurm-out/slurm-%A_%a.out
@@ -19,10 +19,10 @@ SCRIPT="/cosma/home/dp270/dc-glas1/diffuse_emission/vis_realified.py"
 
 echo $@
 
-export OMP_NUM_THREADS=1
-python -u $SCRIPT "$@" -dir=standard \
+export OMP_NUM_THREADS=1 
+python -u $SCRIPT "$@" -dir=standard_a0 \
                        -nsamples=100 \
                        -data_seed=20 \
                        -prior_seed=30 \
-                       -cosmic_variance = True \
+                       -cosmic_var=false \
                        -jobid=$SLURM_ARRAY_TASK_ID
